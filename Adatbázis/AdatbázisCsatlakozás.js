76ac8310aa19d5595d8c806b826cd0564b9a3ed5
Rolidroid0 <122160407+Rolidroid0@users.noreply.github.com>
2024-06-29 10:44:49 +0200

    Adatbázis csatlakozás kiemelése

---
+++ Adatbázis/AdatbázisCsatlakozás.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+
+let dbInstance = null;
+
+async function connectToDb() {
+    if (dbInstance) {
+        return dbInstance;
+    }
+
+    try {
+        const client = await MongoClient.connect(process.env.CONNECTIONSTRING);
+        dbInstance = client.db();
+        console.log('Csatlakozva a MongoDB adatbázishoz');
+        return dbInstance;
+    } catch (err) {
+        console.error('Nem sikerült csatlakozni a MongoDB adatbázishoz', err);
+        throw err;
+    }
+}
+
+module.exports = connectToDb;
